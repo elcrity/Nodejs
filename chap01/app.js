7d079e796fbfa542e858c11f857c30a34e27b23d
elcrity <elcrity@naver.com>
2023-12-21 18:05:24 +0900

    first

---
+++ chap01/app.js
@@ -0,0 +1,31 @@
+import express, { response } from 'express'
+import { readFile } from 'fs'
+
+const app = express()
+
+app.use(express.static('public/img'))
+app.use(express.static('public/css'))
+
+app.get('/', (request, response) => {
+    readFile('chap01/test.html', 'utf-8', (err, data) =>{
+        if(err) {response.send('Not Found File')}
+        response.send(data)
+    })
+})
+
+app.get('/about', (req, res) => {
+    readFile('chap01/about.html', 'utf-8', (err, data) => {
+        if(err) { res.send('No Such File or Directory') }
+        res.send(data)
+    })
+})
+
+
+app.use((req, res, next) => {
+    readFile('chap01/notFound.html', 'utf-8', (err, data) => {
+        if(err) { res.status(404).send('404 Not Found') }
+        res.status(404).send(data)
+    })
+})
+
+app.listen(3000)
