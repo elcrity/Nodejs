4bbe18c22afcb7da21c170d6423376a804bd9fdd
elcrity <elcrity@naver.com>
2024-01-08 18:40:21 +0900

    참조 관계에 대한 주석 추가

--- nodebird/models/user.mjs
+++ nodebird/models/user.mjs
@@ -42,13 +42,19 @@ class User extends Sequelize.Model{
     static associate(db){
         db.User.hasMany(db.Post)
         db.User.belongsToMany(db.User, {
+            //팔로워가 참조하는 키는 팔로우 할 유저의 키이므로 followingId
             foreignKey : 'followingId',
+            //이 별칭은 follower로
             as: 'Followers',
+            //Follow라는 모델을 통해 N:M관계를 정의
             through : 'Follow',
         });
         db.User.belongsToMany(db.User, {
+            //팔로우 된 유저가 참조할 키는 자신을 팔로우 한 유저의 키이므로 followerId
             foreignKey : 'followerId',
+            //이 관계의 별칭은 Followings
             as: 'Followings',
+            //Follow라는 모델을 통해 N:M관계를 정의
             through : 'Follow',
         });
     }
